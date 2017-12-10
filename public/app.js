2f6cfaa2a4c2b513612222c4181458b8826448c4
Findsay <fiona.e.lindsay@gmail.com>
2017-12-10 22:16:23 +0000

    Can catch pokemon" "

--- public/app.js
+++ public/app.js
@@ -22,18 +22,29 @@ var onePokemonRequestComplete = function(){
   var jsonString = this.responseText;
   pokemondetails = JSON.parse(jsonString);
   displayPokemon(pokemondetails);
+};
 
-}
+var wildPokemonComplete = function(){
+  if(this.status !== 200) return;
+  var jsonString = this.responseText;
+  pokemondetails = JSON.parse(jsonString);
+  displayWildPokemon(pokemondetails);
+};
 
 var loadAllPokemon = function(){
   var url = "https://pokeapi.co/api/v2/pokemon/?limit=150";
   makeRequest(url, requestComplete);
-}
+};
 
 function loadAPokemonsDetail(pokemonName){
   var url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
   makeRequest(url, onePokemonRequestComplete);
-}
+};
+
+function loadWildPokemon(pokemonName){
+  var url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+  makeRequest(url, wildPokemonComplete);
+};
 
 function forEachPokemon(array, callback){
   array.forEach(function(pokemon){
@@ -45,34 +56,34 @@ function createNewParagraph(text){
   var newP = document.createElement('p');
   newP.innerText = text;
   return newP;
-}
+};
 
 function createDiv(){
   var newDiv = document.createElement('div');
   return newDiv;
-}
+};
 
 function createImage(url){
   var newImage = document.createElement('img');
   newImage.src = url;
   return newImage;
-}
+};
 
 function createHeader(type, text){
   var newHeader = document.createElement(type);
   newHeader.innerText = text;
   return newHeader;
-}
+};
 
 function createOption(text){
   var newOption = document.createElement('option');
   newOption.innerText = text;
   return newOption;
-}
+};
 
 function appendChild(parent, child){
   parent.appendChild(child);
-}
+};
 
 function displayPokemon(pokemonDetails){
   clearPokemonInfo();
@@ -92,30 +103,30 @@ function displayPokemon(pokemonDetails){
 
   var pPokemonWeight = createNewParagraph(`WEIGHT: ${pokemonDetails.weight}`);
   appendChild(mainDiv, pPokemonWeight);
-}
+};
 
 function popualteSelect(pokemon){
   var mainSelect = document.getElementById('select-pokemon');
   var newOpt = createOption(pokemon.name);
   appendChild(mainSelect, newOpt);
-}
+};
 
 function saveAllPokemon(){
   var jsonString = JSON.stringify(pokemon);
   localStorage.setItem('allPokemon', jsonString);
-}
+};
 
 function clearPokemonInfo(){
   var mainDiv = document.getElementById('pokemon');
   while (mainDiv.firstChild){
     mainDiv.removeChild(mainDiv.firstChild);
   }
-}
+};
 
 function getAllPokemonFromStorage(){
   var jsonString = localStorage.getItem('allPokemon');
   return JSON.parse(jsonString);
-}
+};
 
 function hideButton(button){
   button.style.display = 'none';
@@ -131,17 +142,56 @@ function randomNumber(){
 };
 
 function returnPokemonNamefromInt(index){
-  return pokemon[index].name;
+  return pokemon.results[index].name;
 };
 
-function displayWildPokemon(details){
+function displayWildPokemon(pokemonDetails){
   clearPokemonInfo();
   var mainDiv = document.getElementById('pokemon');
   var pPokemonAppear = createNewParagraph(`A wild ${pokemonDetails.name} appeared!`);
+  pPokemonAppear.id = 'pAppear';
   appendChild(mainDiv, pPokemonAppear);
   var pPokemonImg = createImage(pokemonDetails.sprites.front_shiny);
   appendChild(mainDiv, pPokemonImg);
+  var btnCatch = document.createElement('button');
+  btnCatch.innerText = "Throw Pokeball"
+  appendChild(mainDiv, btnCatch);
+  btnCatch.addEventListener('click', function(){
+    debugger;
+    var chance = returnCatchChance();
+    debugger;
+    if(checkIfCaught(chance)){
+      clearPokemonInfo();
+      var pPokemonCaught = createNewParagraph(`Gotcha! ${pokemonDetails.name} was caught`);
+      pPokemonCaught.id = "pokemonCaught";
+      appendChild(mainDiv, pPokemonCaught);
+    }else{
+      clearPokemonInfo();
+      var pPokemonCaught = createNewParagraph(`Oh no.... ${pokemonDetails.name} escaped!!`);
+      pPokemonCaught.id = "pokemonCaught";
+
+      appendChild(mainDiv, pPokemonCaught);
+
+    }
+  })
+
 };
+
+function returnCatchChance(){
+  return Math.random()
+};
+
+function checkIfCaught(number){
+  if(number < 0.8){
+    return true;
+  }else{
+    return false;
+  }
+};
+
+function displayOutcome(pokemonDetails){
+
+}
 
 // function addToEncountered(){
 //
@@ -153,8 +203,6 @@ function displayWildPokemon(details){
 
 
 var app = function(){
-
-
   // loadAllPokemon();
   pokemon = getAllPokemonFromStorage();
   forEachPokemon(pokemon.results, popualteSelect);
@@ -204,7 +252,12 @@ var app = function(){
     hideButton(btnViewEncountered);
     hideButton(btnViewCaught);
     hideButton(btnPlay);
-    mainSelect.style.display = 'inline';
+
+    var index = randomNumber();
+    var name = returnPokemonNamefromInt(index);
+    loadWildPokemon(name);
+
+
   })
 
 
